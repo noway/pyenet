8653a84fe2b596cc99c2cc386473947d0a764c3a
Way, No <noway@2ch.hk>
2016-12-31 18:14:15 +1300

    more sturdy prebuilt

--- prebuild.sh
+++ prebuild.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
+LIB_NAME=enet-1.3.3
+
 rm -f enet-pyspades.pyx
 rm -f enet
-rm -f enet-1.3.3
+rm -f enet.so
+rm -f "${LIB_NAME}.tar.gz"
+rm -rf "${LIB_NAME}"
 
 cp enet.pyx enet-pyspades.pyx
 git apply pyspades-pyenet.patch
 
 # If this link dies use https://github.com/lsalzman/enet/tree/ee869ab08ade3dae6a6c5ffb0ef1ed1c61d1a709
-wget http://enet.bespin.org/download/enet-1.3.3.tar.gz
+wget "http://enet.bespin.org/download/${LIB_NAME}.tar.gz"
 
-
-tar -xzvf enet-1.3.3.tar.gz
-ln -s enet-1.3.3 enet
+tar -xzvf "${LIB_NAME}.tar.gz"
+ln -s "${LIB_NAME}" enet
 
 git apply pyspades-enet.patch
