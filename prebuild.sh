bb6c9eed580f5b661bfe830ab98d3fed2f91d63b
Way, No <noway@2ch.hk>
2016-12-31 18:47:51 +1300

    there were no differences for enet-1.3.3

--- prebuild.sh
+++ prebuild.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-LIB_NAME=enet-1.3.3
+LIB_VERSION=1.3.3
 
 rm -f enet-pyspades.pyx
 rm -f enet
 rm -f enet.so
-rm -f "${LIB_NAME}.tar.gz"
-rm -rf "${LIB_NAME}"
+rm -f "enet-${LIB_NAME}.tar.gz"
+rm -rf "enet-${LIB_NAME}"
 
 cp enet.pyx enet-pyspades.pyx
 git apply pyspades-pyenet.patch
 
-# If this link dies use https://github.com/lsalzman/enet/tree/ee869ab08ade3dae6a6c5ffb0ef1ed1c61d1a709
-wget "http://enet.bespin.org/download/${LIB_NAME}.tar.gz"
+# If it dies, use "https://github.com/noway421/enet/archive/${LIB_NAME}.tar.gz"
+# But really only https://github.com/noway421/enet/archive/1.3.3.tar.gz exists
+# No other version are supported in pyspades. If their website goes down no
+# other versions are fetchable. If you'd like to port to another version and
+# bespin.org is dead, take a look at https://github.com/lsalzman/enet, although
+# their releases are not correspondent to .tar.gz releases on enet.bespin.org
+wget "http://enet.bespin.org/download/enet-${LIB_NAME}.tar.gz"
 
-tar -xzvf "${LIB_NAME}.tar.gz"
-ln -s "${LIB_NAME}" enet
+tar -xzvf "enet-${LIB_NAME}.tar.gz"
+ln -s "enet-${LIB_NAME}" enet
 
-git apply pyspades-enet.patch
